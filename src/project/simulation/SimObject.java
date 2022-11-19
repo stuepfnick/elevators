03d9465e83e7d451dbb8567a06fab55ad274decc
stuepfnick <stuepfnick@gmx.at>
2022-11-19 15:10:08 +0100

    empty line

--- src/project/simulation/SimObject.java
+++ src/project/simulation/SimObject.java
@@ -3,7 +3,7 @@ package project.simulation;
 import java.awt.*;
 
 public interface SimObject {
-    
+
     void fixedUpdate(double deltaTime);
 
     void render(Graphics2D g, float interpolation);
